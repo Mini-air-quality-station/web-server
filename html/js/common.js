8532ba532ef75511dcc0d1b7442d3fa7e3c90355
punfil <wojtek.panfil@hotmail.com>
2022-06-22 23:00:00 +0200

    configuration: Add option to sync time with user
    Signed-off-by: punfil <wojtek.panfil@hotmail.com>

--- html/js/common.js
+++ html/js/common.js
@@ -6,3 +6,31 @@ function redirect(url, http){
     window.location.href='https://'+url;
   }
 }
+
+function sendUserTime(){
+  var userDate = new Date();
+  var userTime = userDate.getFullYear() + "-" +
+    String(userDate.getMonth()+1) + "-" +
+    userDate.getDate() + " " +
+    userDate.getHours() + ":" +
+    userDate.getMinutes() + ":" +
+    userDate.getSeconds();
+
+  var timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone
+
+  var time = document.createElement('input');
+  time.type = 'hidden';
+  time.name = 'userTime';
+  time.value = userTime;
+
+  var timezone = document.createElement('input');
+  timezone.type = 'hidden';
+  timezone.name = 'userTimezone';
+  timezone.value = timeZone;
+
+  var form = document.getElementById('userTimeForm');
+  form.appendChild(time);
+  form.appendChild(timezone);
+
+  form.submit();
+}
--- html/scripts/README.md
+++ html/scripts/README.md
@@ -2,3 +2,4 @@
 Add to sudoers in `# Allow member of group sudo to execute any command` section
 %www-data ALL=(ALL:ALL) NOPASSWD: /var/www/html/scripts/reboot.sh
 %www-data ALL=(ALL:ALL) NOPASSWD: /var/www/html/scripts/poweroff.sh
+%www-data ALL=(ALL:ALL) NOPASSWD: /var/www/html/scripts/set_date.py
