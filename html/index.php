3051b3ceab8304c1863337347e3953b6a0e88ef4
punfil <wojtek.panfil@hotmail.com>
2023-04-06 21:13:50 +0200

    conf/website: Move menu to mobile app
    Signed-off-by: punfil <wojtek.panfil@hotmail.com>

--- html/index.php
+++ html/index.php
@@ -14,30 +14,11 @@
   </head>
 
   <body>
-
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a class="navbar-brand" href="#">Mini Air Quality Station</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item">
-            <a class="nav-link" href="configuration.php">Configuration</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="about.php">About device</a>
-          </li>
-        </ul>
-      </div>
-    </nav>
-
     <main role="main" class="container">
 
       <div class="starter-template">
         <h1>Welcome to Mini Air Quality Station configuration!</h1>
-        <p class="lead">You can show info about the device and configure it.</p>
+        <p class="lead">You can show info about the device, configure it and display measurements.</p>
         <!-- TODO: Add picture of the station -->
       </div>
 
